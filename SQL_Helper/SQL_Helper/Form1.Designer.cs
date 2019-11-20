731e25a81e7683962e7aa84fb60b352ddcb88a7f
Renato Canizella <renatocza@hotmail.com>
2019-11-20 18:56:47 -0300

    Initial Commit

---
+++ SQL_Helper/SQL_Helper/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace SQL_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tbTable = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbConn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNamespace = new System.Windows.Forms.TextBox();
+            this.tbClass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.tbTable);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbConn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configurações SQL";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(3, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(794, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Carregar Classe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(539, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Trusted Connection";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tbTable
+            // 
+            this.tbTable.Location = new System.Drawing.Point(116, 52);
+            this.tbTable.Name = "tbTable";
+            this.tbTable.Size = new System.Drawing.Size(249, 20);
+            this.tbTable.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tabela";
+            // 
+            // tbConn
+            // 
+            this.tbConn.Location = new System.Drawing.Point(116, 26);
+            this.tbConn.Name = "tbConn";
+            this.tbConn.Size = new System.Drawing.Size(672, 20);
+            this.tbConn.TabIndex = 3;
+            this.tbConn.Text = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password = myPasswo" +
+    "rd;";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connection String";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(800, 272);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Configurações Classe";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton2);
+            this.groupBox4.Controls.Add(this.radioButton1);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox4.Location = new System.Drawing.Point(245, 16);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(120, 253);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Construtores";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Private";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Public";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(242, 253);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbClass);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.tbNamespace);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(365, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(432, 253);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Propriedades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Namespace";
+            // 
+            // tbNamespace
+            // 
+            this.tbNamespace.Location = new System.Drawing.Point(76, 28);
+            this.tbNamespace.Name = "tbNamespace";
+            this.tbNamespace.Size = new System.Drawing.Size(217, 20);
+            this.tbNamespace.TabIndex = 1;
+            // 
+            // tbClass
+            // 
+            this.tbClass.Location = new System.Drawing.Point(76, 53);
+            this.tbClass.Name = "tbClass";
+            this.tbClass.Size = new System.Drawing.Size(217, 20);
+            this.tbClass.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Classe";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Gerar Classe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox tbTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbClass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNamespace;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SQL_Helper/SQL_Helper/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Clipboard.SetText("Server=.\\SQLExpress;Database=iris;Trusted_Connection=True;");
+            InitializeComponent();
+        }
+
+        private string code = "";
+        private Dictionary<string, string> colunas = new Dictionary<string, string>();
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            tbConn.Text = !checkBox1.Checked
+                ? "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password = myPassword;"
+                : "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            colunas.Clear();
+            string text = tbConn.Text;
+            StringBuilder stringBuilder = new StringBuilder();
+            if (!string.IsNullOrEmpty(text))
+            {
+                using (SqlConnection connection = new SqlConnection(text))
+                {
+                    connection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand("sp_columns '" + tbTable.Text + "'", connection))
+                    {
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                string str = sqlDataReader["COLUMN_NAME"].ToString();
+                                string lower = sqlDataReader["IS_NULLABLE"].ToString().ToLower();
+                                string type1 = GetType(sqlDataReader["TYPE_NAME"].ToString());
+                                string type2 = type1 + (!(lower == "yes") || !(type1 != "string") ? "" : "?");
+                                stringBuilder.AppendLine(FetchField(str, type2));
+                                colunas.Add(str, type2);
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            this.checkedListBox1.Items.AddRange((object[])this.colunas.Keys.ToArray<string>());
+            this.code = stringBuilder.ToString();
+        }
+
+        private string FetchField(string column, string type)
+        {
+            return (!this.radioButton1.Checked
+                ?
+                "\t\tprivate [type] _[column];" +
+                "\n\n" +
+                "\t\tpublic [type] [column]\n" +
+                "\t\t{\n" +
+                "\t\t\tget { return _[column]; }\n" +
+                "\t\t\tset { _[column] = value; }\n" +
+                "\t\t}"
+                :
+                "\t\tpublic [type] [column] { get; set; }")
+                .Replace("[type]", type)
+                .Replace("[column]", column)
+                + "\n\n";
+        }
+
+        public string GetType(string type)
+        {
+            type = type.Replace("identity", "").Trim();
+            string lower = type.ToLower();
+            if (lower != null)
+            {
+                switch (lower)
+                {
+                    case "bigint":
+                        return "long";
+                    case "binary":
+                    case "image":
+                    case "timestamp":
+                    case "varbinary":
+                        return "byte[]";
+                    case "bit":
+                        return "bool";
+                    case "char":
+                    case "nchar":
+                    case "ntext":
+                    case "nvarchar":
+                    case "text":
+                    case "varchar":
+                    case "xml":
+                        return "string";
+                    case "date":
+                    case "datetime":
+                    case "datetime2":
+                    case "smalldatetime":
+                    case "time":
+                        return "DateTime";
+                    case "datetimeoffset":
+                        return "DateTimeOffset";
+                    case "decimal":
+                    case "money":
+                    case "smallmoney":
+                        return "decimal";
+                    case "float":
+                        return "double";
+                    case "int":
+                        return "int";
+                    case "real":
+                        return "float";
+                    case "smallint":
+                        return "short";
+                    case "tinyint":
+                        return "short";
+                    case "uniqueidentifier":
+                        return "Guid";
+                }
+            }
+            return "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> list = this.checkedListBox1.CheckedItems.Cast<string>().ToList<string>();
+            StringBuilder stringBuilder = new StringBuilder();
+            List<string> stringList = new List<string>();
+            stringBuilder.AppendLine("\t\tpublic [class][header]");
+            stringBuilder.AppendLine("\t\t{");
+            foreach (string key in list)
+            {
+                if (this.colunas.ContainsKey(key))
+                {
+                    string valueOrDefault = colunas[key];
+                    stringBuilder.AppendLine("\t\t\tthis." + key + " = " + key + ";");
+                    stringList.Add(valueOrDefault + " " + key);
+                }
+            }
+            stringBuilder.AppendLine("\t\t}");
+            stringBuilder.AppendLine(Environment.NewLine);
+            string newValue = stringBuilder.ToString().Replace("[class]", tbClass.Text).Replace("[header]", "(" + string.Join(", ", (IEnumerable<string>)stringList) + ")");
+            string str = "";
+            if (File.Exists(Path.Combine(Application.StartupPath, "BaseClass.txt")))
+                str = File.ReadAllText(Path.Combine(Application.StartupPath, "BaseClass.txt"));
+            new Form2(str.Replace("[code]", code).Replace("[class]", tbClass.Text).Replace("[constructor]", newValue).Replace("[namespace]", this.tbNamespace.Text)).Show();
+        }
+    }
+}
